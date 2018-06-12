bdb6971c3c9a9573fb6f4d8329f654f3919ca8e4
Sung Rim Huh <sungrimhuh@gmail.com>
2018-06-12 13:41:49 -0700

    add 0001-two-sum.c
    LeetCode loves bolds and italics.

---
+++ 0001-two-sum.c
@@ -0,0 +1,41 @@
+/**
+ * # Problem
+ *
+ * Given an array of integers, return __indices__ of the two numbers such that
+ * they add up to a specific target.
+ *
+ * You may assume that each input would have ___exactly___ one solution, and you
+ * may not use the _same_ element twice.
+ *
+ * Note: The returned array must be malloced, assume caller calls free().
+ *
+ * # Explanation
+ *
+ * This is a straightforward solution with nested loop. The outer loop finds the
+ * first index and the inner loop finds the second index.
+ *
+ * ## Time complexity
+ *
+ * Since there are `N choose 2`, `N * (N - 1) / 2`, such combinations of inputs,
+ * the time complexity is O(N^2), where `N` is the number of elements.
+ *
+ * ## Memory complexity
+ *
+ * There is no additional memory required besides the indices, so the memory
+ * complexity is O(1).
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+
+    for (int i = 0; i < numsSize; ++i) {
+        for (int j = i + 1; j < numsSize; ++j) {
+            if (nums[i] + nums[j] == target) {
+                int* ret = malloc(sizeof(int) * 2);
+                ret[0] = i;
+                ret[1] = j;
+                return ret;
+            }
+        }
+    }
+
+    return NULL;
+}
